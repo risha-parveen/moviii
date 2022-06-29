fde52f5f59375be5caeea6325d15f20fec19af87
risha-parveen <rishaparveen12@gmail.com>
2022-06-29 18:08:57 +0530

    front page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
-    <img src="/moviii_logo.png" alt="moviii logo" >
-    <nuxt-link to="dashboard" >Get Started</nuxt-link>
+    <img src="/big_moviii.png" alt="moviii logo" width="600px">   
+      <nuxt-link to="dashboard" >
+        <button>Get Started</button>
+      </nuxt-link>
   </div>
 </template>
 
@@ -11,10 +13,44 @@ export default {
 }
 </script>
 
-<style lang="scss" >
+<style lang="scss">
+  $primary:#201B1D;
+  $color:#B25656;
+  $dark-color:#a13838;
+  
   html,.root,.body,.container{
-    background-color:#201B1D;
+    background-color:$primary;
   }
 
+  .container{
+    margin-top:250px;
+    text-align: center;
+    align-items: center;
+    display:flex;
+    flex-direction: column;
 
+    img{
+      padding:0;
+    }
+
+    a{
+        transition:0.4s ease;
+        margin:30px;
+
+        button{
+          border-radius: 10px;
+          border:none;
+          padding:10px 15px;
+          transition:0.4s ease;
+          background-color:$color;
+          font-size:1.2rem;
+          color:white;
+
+          &:hover{
+            transform:scale(1.05);
+            background-color:darken($color,10);
+          }
+        }
+      } 
+  }
 </style>
