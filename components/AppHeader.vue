b7c114682358278624be381c1fa3deafa7609cb3
risha-parveen <rishaparveen12@gmail.com>
2022-06-29 07:56:36 +0530

    added files

---
+++ components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <img src="/moviii_logo.png" alt="moviii logo">
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+header{
+  background-color:#201B1D;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <img src="/moviii_logo.png" alt="moviii logo" >
+    <nuxt-link to="dashboard" >Get Started</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="scss" >
+  html,.root,.body,.container{
+    background-color:#201B1D;
+  }
+
+
+</style>
